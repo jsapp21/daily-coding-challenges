14c6f4f1a5f2f13fd5290dcc41f0216a24c3ccad
James Sapp <jameseisapp@gmail.com>
2021-04-20 11:22:44 -0500

    tuesday

---
+++ javascript/jadenCaseStr.spec.js
@@ -0,0 +1,14 @@
+const toTitleCase = (str) => {
+  return str
+    .toLowerCase()
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+};
+
+
+describe('jaden case strings', () => {
+    test('one', () => {
+      (toTitleCase("How can mirrors be real if our eyes aren't real"), "How Can Mirrors Be Real If Our Eyes Aren't Real");
+    });
+  });
