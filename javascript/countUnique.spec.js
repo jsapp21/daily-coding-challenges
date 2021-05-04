65fc20e841aa9579178d6b8d1a8fd31380b1a4a7
James Sapp <jameseisapp@gmail.com>
2021-05-04 13:11:04 -0500

    countUnique

---
+++ javascript/countUnique.spec.js
@@ -0,0 +1,43 @@
+// create a function that accpets a sorted array and counts the unique values, return that number
+// O(n) b/c it only loops once 
+
+const arr = [1, 2, 3, 4, 7, 7, 12, 12, 12, 13]
+
+const countUniqueValues = (arr) => {
+    let i=0;
+
+    for (let j=0; j < arr.length; j++){
+       if (arr[i] !== arr[j]) {
+           i++;
+           arr[i] = arr[j]
+       }
+    }
+
+    return i + 1;
+
+
+}
+
+console.log(countUniqueValues(arr))
+
+
+// #################  MY SOLUTION ##############
+
+// if (arr.length <= 1) {
+//     return arr.length
+// }
+// let count = {};
+
+// for (let i=0; i < arr.length; i++){
+//     let key = arr[i];
+
+//     if (count[key] > 0){
+//         count[key]++
+//     } else {
+//         count[key] = 1
+//     }
+// }
+
+// let values = Object.values(count)
+
+// return values.length
